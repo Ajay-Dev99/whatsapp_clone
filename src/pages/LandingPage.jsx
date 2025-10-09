ce4759aec65bdc105fd439558d4c60d82e76e0e0
ajaydev <ajaydevkomath@gmail.com>
2025-10-09 23:25:12 +0530

    fix: Remove unnecessary console log from LandingPage component

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -12,7 +12,6 @@ export default function LandingPage() {
             <p className='text-[#909090] text-[1rem]'>A simple , reliable , and private way to  use WhatsApp on your computer</p>
             <button className='bg-[#1daa61] px-[3rem] py-1 rounded-sm text-sm my-3 cursor-pointer' onClick={() => navigate("/home")}>Get Started</button>
             <p className='text-[#909090] text-[1rem]'>Version 2.2535.3.0</p>
-            console.log("slfjkdsafklj")
         </div>
     )
 }
